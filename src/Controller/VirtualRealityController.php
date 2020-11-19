5b288deb70255649637b1ab6920184410040b607
server <marlonapolop2794@gmail.com>
2020-11-19 08:50:39 -0800

    Uniendo en un solo controlador para realidad virtual

--- src/Controller/VirtualRealityController.php
+++ src/Controller/VirtualRealityController.php
@@ -11,8 +11,24 @@ class VirtualRealityController extends AbstractController
     /**
      * @Route("/virtual-reality", name="virtual_reality")
      */
-    public function index(): Response
+    public function virtualReality(): Response
     {
         return $this->render('virtual_reality/index.html.twig');
+    }
+
+    /**
+     * @Route("/avatar", name="avatar")
+     */
+    public function avatar(): Response
+    {
+        return $this->render('virtual_reality/avatar.html.twig');
+    }
+
+    /**
+     * @Route("/chatvr", name="chatvr")
+     */
+    public function chatVr(): Response
+    {
+        return $this->render('virtual_reality/chat-vr.html.twig');
     }
 }
